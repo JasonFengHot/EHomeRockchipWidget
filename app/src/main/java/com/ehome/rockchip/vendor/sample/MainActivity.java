3c9f812b99329109d8130799efc3750bd3b24e96
冯辉斌 <fenghuibin1591@dingtalk.com>
2022-03-11 14:35:13 +0800

    Fix #N/A 验证血常规老平板序列号是否能够写入成功

--- app/src/main/java/com/ehome/rockchip/vendor/sample/MainActivity.java
+++ app/src/main/java/com/ehome/rockchip/vendor/sample/MainActivity.java
@@ -5,23 +5,34 @@ import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
 
 import com.ehome.rockchip.vendor.RockchipStorageManager;
 
 public class MainActivity extends AppCompatActivity {
     private RockchipStorageManager mManager;
 
+    private AppCompatTextView mTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.text);
+
         mManager = new RockchipStorageManager();
     }
 
     public void get(View view) {
-       String sn =  mManager.getSerialNumber();
 
+        String sn = mManager.getSerialNumber();
+        mTextView.setText(sn);
         Log.d("RockchipStorageManager", "sn -> " + sn);
+    }
 
+    public void set(View view) {
+        String sn = "1122334455";
+        mManager.putSerialNumber(sn);
+        Log.d("RockchipStorageManager", "sn -> " + sn);
     }
 }
